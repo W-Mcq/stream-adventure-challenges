1d4e96d0f46cf0cd3f029398908e47daf4ca161e
U-Lovelace\Hobbes <Hobbes@Lovelace.(none)>
2014-11-04 21:38:01 -1000

    solved the transform problem

---
+++ transform.js
@@ -0,0 +1,9 @@
+/*jslint node: true*/
+"use strict";
+var through = require("through");
+var tr = through(function write(data) {
+  if (data) {
+    this.queue(data.toString().toUpperCase());
+  }
+});
+process.stdin.pipe(tr).pipe(process.stdout);
